f710d7256036a9cf88427398a7767661ed1a098c
ronelsutabinto <ronels1122@gmail.com>
2023-02-07 10:29:49 -0800

    Create razor web page and implement css bootstrap framework

--- ShepherdPOS.Web/Program.cs
+++ ShepherdPOS.Web/Program.cs
@@ -2,12 +2,20 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShepherdPOS.Web;
 
+//using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ShepherdPOS.Web;
+using ShepherdPOS.Web.Services;
+using ShepherdPOS.Web.Services.Contracts;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); Temporarily disable this statement blazor loader
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7282/") });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
 
