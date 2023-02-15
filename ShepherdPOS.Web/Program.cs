83047e548e1968674cbc271a10d8b0609f5def50
ronelsutabinto <ronels1122@gmail.com>
2023-02-15 08:58:37 -0800

    Add Web app dashboard content with mudblazor

--- ShepherdPOS.Web/Program.cs
+++ ShepherdPOS.Web/Program.cs
@@ -1,20 +1,24 @@
-﻿using Blazored.LocalStorage;
+﻿
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShepherdPOS.Web;
+
+using Blazored.LocalStorage;
 using ShepherdPOS.Web.Services;
 using ShepherdPOS.Web.Services.Contracts;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); Temporarily disable this statement blazor loader
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); Temporarily disable this statement blazor loader 7282
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7282/") });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IPosCartService, PosCartService>();
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped<IManageProductsLocalStorageService, ManageProductsLocalStorageService>();
 builder.Services.AddScoped<IManageCartItemsLocalStorageService, ManageCartItemsLocalStorageService>();
